3bb42625a5f8ddb7f21bc7f34db9320066122134
Juan Eugenio <juaneugeniopop@gmail.com>
2022-08-25 11:58:25 -0500

    Primer programa del curso, listas y diccionarios anidados

---
+++ lists_and_dicts.py
@@ -0,0 +1,28 @@
+from multiprocessing.sharedctypes import Value
+
+
+def run():
+    my_list = [1,"Hello",True,4.5]
+    my_dict = {"firstname":"Juan","lastname":"Eugenio"}
+
+    super_list = [
+        {"firstname":"Juan","lastname":"Eugenio"},
+        {"firstname":"Miguel","lastname":"Morin"},
+        {"firstname":"Jose","lastname":"Garcia"},
+        {"firstname":"Susana","lastname":"Martinez"},
+        {"firstname":"Pepe","lastname":"Rodelo"}    
+    ]
+
+    super_dict = {
+        "natural_nums" : [1,2,3,4,5],
+        "integer_nums" : [-1,-2,0,1,2],
+        "floating_nums" : [1.1,4.5,6.43]
+    }
+
+    for key, value in super_dict.items():
+        print(key,"-",value)
+
+
+
+if __name__ == '__main__':
+    run()
